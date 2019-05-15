1e9f9b258c9d4356bcbe9e4cdc402c6f16f10bd7
Bgg <klfn_wdwl_1@163.com>
2019-05-15 23:42:03 +0800

    第二次提交到本地仓库

--- src/GitConnect.java
+++ src/GitConnect.java
@@ -2,5 +2,7 @@ public class GitConnect {
     public static void main(String[] args) {
         System.out.println("bgg,haodiao");
         System.out.println("bgg,haodiao2");
+        System.out.println("bgg,haodiao5");
+        System.out.println("bgg,haodiao6");
     }
 }
